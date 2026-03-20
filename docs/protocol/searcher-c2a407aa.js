df957fada7dd8a75f8650075563bd6e9a54f6f6c
Robert Quattlebaum <darco@deepdarc.com>
2026-03-20 16:00:48 -0700

    Update GitHub Pages

--- docs/protocol/searcher-c2a407aa.js
+++ docs/protocol/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-30d4af89.js',
+                path_to_root + 'searchindex-d68eaf2f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
