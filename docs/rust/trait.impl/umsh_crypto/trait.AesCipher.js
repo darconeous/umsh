ddbd73ab69de96e48540e03acf256a66b13ddb75
Robert Quattlebaum <darco@deepdarc.com>
2026-04-10 20:06:17 -0700

    Update GitHub Pages

--- docs/rust/trait.impl/umsh_crypto/trait.AesCipher.js
+++ docs/rust/trait.impl/umsh_crypto/trait.AesCipher.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["umsh_crypto",[]],["umsh_mac",[["impl AesCipher for <a class=\"struct\" href=\"umsh_mac/test_support/struct.DummyCipher.html\" title=\"struct umsh_mac::test_support::DummyCipher\">DummyCipher</a>",0]]]]);
+    const implementors = Object.fromEntries([["umsh_crypto",[]],["umsh_mac",[["impl <a class=\"trait\" href=\"umsh_crypto/trait.AesCipher.html\" title=\"trait umsh_crypto::AesCipher\">AesCipher</a> for <a class=\"struct\" href=\"umsh_mac/test_support/struct.DummyCipher.html\" title=\"struct umsh_mac::test_support::DummyCipher\">DummyCipher</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[18,184]}
+//{"start":59,"fragment_lengths":[18,288]}
