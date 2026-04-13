0f47f8fc88d53f7826354de7b1c2f8df43480f44
Robert Quattlebaum <darco@deepdarc.com>
2026-04-12 17:01:03 -0700

    Update GitHub Pages

--- docs/rust/trait.impl/umsh_hal/trait.Radio.js
+++ docs/rust/trait.impl/umsh_hal/trait.Radio.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["umsh",[["impl <a class=\"trait\" href=\"umsh_hal/trait.Radio.html\" title=\"trait umsh_hal::Radio\">Radio</a> for <a class=\"struct\" href=\"umsh/tokio_support/struct.UdpMulticastRadio.html\" title=\"struct umsh::tokio_support::UdpMulticastRadio\">UdpMulticastRadio</a>",0]]],["umsh_mac",[["impl Radio for <a class=\"struct\" href=\"umsh_mac/test_support/struct.ModeledRadio.html\" title=\"struct umsh_mac::test_support::ModeledRadio\">ModeledRadio</a>",0],["impl Radio for <a class=\"struct\" href=\"umsh_mac/test_support/struct.SimulatedRadio.html\" title=\"struct umsh_mac::test_support::SimulatedRadio\">SimulatedRadio</a>",0]]]]);
+    const implementors = Object.fromEntries([["umsh",[["impl <a class=\"trait\" href=\"umsh_hal/trait.Radio.html\" title=\"trait umsh_hal::Radio\">Radio</a> for <a class=\"struct\" href=\"umsh/tokio_support/struct.UdpMulticastRadio.html\" title=\"struct umsh::tokio_support::UdpMulticastRadio\">UdpMulticastRadio</a>",0]]],["umsh_mac",[["impl <a class=\"trait\" href=\"umsh_hal/trait.Radio.html\" title=\"trait umsh_hal::Radio\">Radio</a> for <a class=\"struct\" href=\"umsh_mac/test_support/struct.ModeledRadio.html\" title=\"struct umsh_mac::test_support::ModeledRadio\">ModeledRadio</a>",0],["impl <a class=\"trait\" href=\"umsh_hal/trait.Radio.html\" title=\"trait umsh_hal::Radio\">Radio</a> for <a class=\"struct\" href=\"umsh_mac/test_support/struct.SimulatedRadio.html\" title=\"struct umsh_mac::test_support::SimulatedRadio\">SimulatedRadio</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[277,357]}
+//{"start":59,"fragment_lengths":[277,537]}
